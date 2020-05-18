accbbcf511bc32989707fe6953bc21fd57b76677
D12 <D12@gmail.com>
2020-05-18 19:01:26 +0530

    =

--- program.py
+++ program.py
@@ -15,13 +15,29 @@ registers = {"r0":"",
 "r12":""
 }
 
+# function to return key for any value 
+def get_key(val): 
+	for key, value in registers.items(): 
+		if val == value: 
+			return key 
+
+	return "key doesn't exist"
+
+print(get_key(100)) 
+print(get_key(11)) 
+
+
 output_list = []
 
-def get_a_free_register():
+def get_a_free_register(inside):
     for regs in registers:
         if(registers[regs] == ""):
+            registers[regs] = inside
             return regs
 #do be completed later
+
+temp_res = {}
+data = []
 
 filename = sys.argv[1]
 f = open(filename,"r")
@@ -32,10 +48,31 @@ for row in rows:
     # list_quad = 0 operand 1 arg1 2 arg2 3 result
     #print(list_quad)
     if(list_quad[0] == "="):
-        if(list_quad[1].isdigit()):
-            reg = get_a_free_register()
-            output_list.append(["mov "+reg+","+str(hex(int(list_quad[1])))])
-        
+        if(list_quad[1][0] == "T"):
+            variable = temp_res[list_quad[1]]
+        elif(list_quad[1].isdigit()):
+            if(list_quad[1] not in registers.values()):
+                reg = get_a_free_register(list_quad[1])
+                output_list.append(["mov "+reg+","+str(hex(int(list_quad[1])))])
+                variable = reg
+            else:
+                variable = get_key(list_quad[1])
+        else:
+            if(list_quad[1] not in data):
+                data.append(list_quad[1])
+            reg = get_a_free_register(list_quad[1])
+            output_list.append("ldr "+reg+","+"="+list_quad[1])
+            output_list.append("ldr "+reg+","+"["+reg+"]")
+            variable = reg
+        if(list_quad[3] not in data):
+            data.append(list_quad[3])
+        if(list_quad[3] not in registers.values()):
+            reg = get_a_free_register((list_quad[3]))
+            output_list.append("ldr "+reg+","+"="+list_quad[3])
+            output_list.append("ldr "+variable+","+"["+reg+"]")
+        else:
+            reg = get_key(list_quad[3])
+            output_list.append("ldr "+variable+","+"["+reg+"]")
 
 
 print(output_list)
